91baf25fccc208262e83a65c31594e58f0cfa2aa
natsabari <natsabari@gmail.com>
2014-09-17 01:47:54 +0530

    Revert "Cache matrix"
    This reverts commit e2cb13d6daae480bce92f2eb65ee9e8d09ad18ee.

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  so <- NULL
-  set <- function(y) {
-    x <<- y
-    so <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve ) so <<- solve
-  getsolve <- function() so
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+
 }
 
 
@@ -22,13 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  so <- x$getsolve()
-  if(!is.null(so)) {
-    message("getting cached data")
-    return(so)
-  }
-  data <- x$get()
-  so <- solve(data, ...)
-  x$setsolve(so)
-  so
 }
